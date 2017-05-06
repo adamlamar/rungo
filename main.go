a18d73cff3a081f7e2b9337d86f7112f26f8e7b2
Adam Lamar <adamonduty@gmail.com>
2017-05-06 12:08:29 -0600

    Set GOROOT

--- main.go
+++ main.go
@@ -86,6 +86,7 @@ func main() {
 	}
 
 	// Run go command
+	setGoRoot(baseDir)
 	if len(flag.Args()) > 1 {
 		err = runGo(baseDir, flag.Args()[1:])
 	} else {
@@ -113,6 +114,10 @@ func fileExists(path string) bool {
 		return false
 	}
 	return true
+}
+
+func setGoRoot(baseDir string) {
+	os.Setenv("GOROOT", baseDir)
 }
 
 func downloadFile(url, fileToSave string) error {
